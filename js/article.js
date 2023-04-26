f51ba782ff4f3710bc6094a7aff7b6b11fe5a3ca
Christian Panter <pantercp@outlook.com>
2023-04-26 23:29:33 +0100

    added javascript for table of contents

---
+++ js/article.js
@@ -0,0 +1,9 @@
+const ToCbtn = document.querySelector(".drop-table");
+const ToClist = document.querySelector('.toc-list');
+
+ToCbtn.addEventListener('click', () => {
+  ToCbtn.classList.toggle('open');
+  ToClist.classList.toggle('open');
+});
+
+alert("I am here!");
