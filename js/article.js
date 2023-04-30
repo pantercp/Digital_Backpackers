cbe1fadc8556713cea1643492a32829a5ef38a66
Christian Panter <pantercp@outlook.com>
2023-04-30 14:44:05 +0100

    Added accordion for budgeting tips

--- js/article.js
+++ js/article.js
@@ -5,3 +5,10 @@ ToCbtn.addEventListener('click', () => {
   ToCbtn.classList.toggle('open');
   ToClist.classList.toggle('open');
 });
+
+const Tipsbtn = document.querySelector(".tip-header");
+const Tipsdetail = document.querySelector(".budgeting-tips");
+
+Tipsbtn.addEventListener('click', () => {
+  Tipsdetail.classList.toggle('open');
+});
